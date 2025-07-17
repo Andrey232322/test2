fb4d0e5c088b51d7c125c43c2c5af7b3dc08dff4
Your Name <you@example.com>
2025-07-17 17:15:42 +0300

    1

--- project/myapp/urls.py
+++ project/myapp/urls.py
@@ -3,11 +3,10 @@ from rest_framework.routers import DefaultRouter
 from .views import UserProfileViewSet, UserView
 
 profiles = DefaultRouter()
-user = DefaultRouter()
+
 profiles.register(r'profiles', UserProfileViewSet, basename='profile')
-user.register(r'user', UserView, basename='user')
+profiles.register(r'user', UserView, basename='user')
 
 urlpatterns = [
     path('', include(profiles.urls)),
-    path('', include(user.urls)),
 ]
